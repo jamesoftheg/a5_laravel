3907740f23106dc82635850b28e6eed95a9a4c1f
jamesoftheg <gelfandjames@gmail.com>
2021-03-26 00:26:32 -0400

    Updated

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\RoomsController;
+use App\Http\Controllers\BookingsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,7 @@ Route::get('/', [PagesController::class,"index"]);
 Route::get('about', [PagesController::class,"about"]);
 Route::get('services', [PagesController::class,"services"]);
 
-Route::resource('rooms', RoomsController::class);
+Route::resource('/rooms', RoomsController::class);
 //Route::resource('rooms', 'RoomsController');
 // We can view our routes using php artisan route:list in the terminal
 
