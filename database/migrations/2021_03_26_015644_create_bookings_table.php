a51d9cc093a18132d9d06de5f6081ac8c5019c06
jamesoftheg <gelfandjames@gmail.com>
2021-03-27 21:30:45 -0400

    Updated

--- database/migrations/2021_03_26_015644_create_bookings_table.php
+++ database/migrations/2021_03_26_015644_create_bookings_table.php
@@ -15,7 +15,7 @@ class CreateBookingsTable extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
-            $table->integer('room_id');
+            $table->foreignId('room_id')->references('id')->on('rooms');
             $table->integer('room_number');
             $table->string('room_name');
             $table->string('guest_name');
